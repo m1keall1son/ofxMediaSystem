4adc736b09666b49c1e9b756d9bc9d8ec449fb70
Michael Allison <hello@michaelallison.lol>
2018-06-11 11:57:59 -0400

    added license and addon config

---
+++ addon_config.mk
@@ -0,0 +1,6 @@
+meta:
+	ADDON_NAME = ofxMediaSystem
+	ADDON_DESCRIPTION = A simple, extensible scene-based data structure for organizing behavior and presentation 
+	ADDON_AUTHOR = Michael Allison
+	ADDON_TAGS = "media"
+	ADDON_URL = https://github.com/m1keall1son/ofxMediaSystem
