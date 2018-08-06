ecda2bdd1d99a87d1be585d2f722e158e219a1fe
Michael Allison <hello@michaelallison.lol>
2018-08-06 17:19:06 -0400

    added actual named layers with their own presenters

--- src/mediasystem/rendering/LayeredRenderer.hpp
+++ src/mediasystem/rendering/LayeredRenderer.hpp
@@ -10,216 +10,249 @@
 #include <tuple>
 #include "ofMain.h"
 #include "mediasystem/core/Entity.h"
-#include "mediasystem/core/ComponentSystem.hpp"
 #include "mediasystem/rendering/IPresenter.h"
 #include "mediasystem/rendering/DefaultPresenter.h"
 #include "mediasystem/util/TupleHelpers.hpp"
 
 namespace mediasystem {
+
+template<typename T>
+class Drawable : public T {
+public:
     
-    template<typename T>
-    class Drawable : public T {
-    public:
-        
-        template<typename...Args>
-        Drawable(Entity& entity, Args&&...args):
+    template<typename...Args>
+    Drawable(Entity& entity, Args&&...args):
         T(std::forward<Args>(args)...),
         mEntity(entity)
-        {}
-        
-        //setters
-        void setLayer(float layer){ mLayer = layer; }
-        void hide(){ mVisible = false; }
-        void show(){ mVisible = true; }
-        void setColor(const ofFloatColor& color){ mColor = color; }
-        void setAlpha(float alpha){ mAlpha = alpha; }
-        
-        //layered concept
-        float getLayer() const { return mLayer; }
-        bool isVisible()const{ return mVisible; }
-        const ofFloatColor& getColor()const{ return mColor; }
-        float getAlpha() const { return mAlpha; }
-        glm::mat4 getGlobalTransformMatrix(){
-            if(auto node = mEntity.getComponent<ofNode>()){
-                return node->getGlobalTransformMatrix();
-            }
-            return glm::mat4();
+    {}
+    
+    //setters
+    void setLayer(std::string layer){ mLayer = std::move(layer); }
+    void setDrawOrder(float order){ mDrawOrder = order; }
+    void hide(){ mVisible = false; }
+    void show(){ mVisible = true; }
+    void setColor(const ofFloatColor& color){ mColor = ofFloatColor(color.r,color.g,color.b,mColor.a); }
+    void setAlpha(float alpha){ mColor.a = alpha; }
+    
+    //layered concept
+    const std::string& getLayer() const { return mLayer; }
+    float getDrawOrder() const { return mDrawOrder; }
+    bool isVisible()const{ return mVisible; }
+    const ofFloatColor& getColor()const{ return mColor; }
+    float getAlpha() const { return mColor.a; }
+    glm::mat4 getGlobalTransformMatrix(){
+        if(auto node = mEntity.getComponent<ofNode>()){
+            return node->getGlobalTransformMatrix();
         }
-        
-        //drawable concept
-        void draw(){ T::draw(); }
-        
-    private:
-        Entity& mEntity;
-        ofFloatColor mColor{1.,1.,1.};
-        float mAlpha{1};
-        float mLayer{0.f};
-        bool mVisible{true};
+        return glm::mat4();
+    }
+    
+    //drawable concept
+    void draw(){ T::draw(); }
+    
+private:
+    Entity& mEntity;
+    ofFloatColor mColor{1.,1.,1.,1.};
+    float mDrawOrder{0.f};
+    std::string mLayer{"default"};
+    bool mVisible{true};
+};
+
+template<typename...DrawableTypes>
+class LayeredRenderer {
+    
+    using TypesList = std::tuple<std::list<std::weak_ptr<Drawable<DrawableTypes>>>...>;
+    using OrderedLayer = std::map<float /* draw order */, TypesList>;
+    struct Layer {
+        Layer( std::string _name, std::shared_ptr<IPresenter> _presenter, float order = 0.f ):
+            name(std::move(_name)),
+            presenter(std::move(_presenter))
+        {}
+        std::string name;
+        float order{0.f};
+        OrderedLayer layer;
+        std::shared_ptr<IPresenter> presenter;
+    };
+    using LayerList = std::list<Layer>;
+    
+public:
+    
+    LayeredRenderer(Scene& scene):
+    mScene(scene)
+    {
+        mLayers.emplace_back("default", std::make_shared<DefaultPresenter>(), 0.f);
+        mScene.addDelegate<Draw>(EventDelegate::create<LayeredRenderer,&LayeredRenderer::onDraw>(this));
+        int l[] = {(addNewComponentDelegate<DrawableTypes>(),0)...};
+        (void)l;
+    }
+    
+    ~LayeredRenderer(){
+        mScene.removeDelegate<Draw>(EventDelegate::create<LayeredRenderer,&LayeredRenderer::onDraw>(this));
+        int l[] = {(removeNewComponentDelegate<DrawableTypes>(),0)...};
+        (void)l;
+    }
+
+    void addLayer(std::string name, std::shared_ptr<IPresenter> presenter = std::make_shared<DefaultPresenter>(), float order = 0.f){
+        auto found = std::find_if(mLayers.begin(), mLayers.end(), [order](const Layer& layer){
+            return layer.order >= order;
+        });
+        if(found != mLayers.end()){
+            mLayers.emplace(found, std::move(name), std::move(presenter), order);
+        }else{
+            mLayers.emplace_back(std::move(name), std::move(presenter), order);
+        }
+    }
+    
+    void setDefault(std::shared_ptr<IPresenter> presenter, float order = 0.f){
+        auto found = std::find_if(mLayers.begin(), mLayers.end(), [](const Layer& layer){
+            return layer.name == "default";
+        });
+        found->presenter = presenter;
+        found->order = order;
+    }
+    
+    void draw(){
+        for(auto & layer : mLayers){
+            for ( auto & order : layer.layer ) {
+                for_each_in_tuple(order.second,OrderChecker<DrawableTypes...>(*this, layer.name, order.first));
+            }
+        }
+        for(auto & layer : mLayers){
+            layer.presenter->begin();
+            for ( auto & order : layer.layer ) {
+                for_each_in_tuple(order.second,LayerDrawer<DrawableTypes...>(*this));
+            }
+            layer.presenter->end();
+        }
+        for(auto & layer : mLayers){
+            layer.presenter->present();
+        }
+    }
+    
+    void setGlobalAlpha(float alpha){ mGlobalAlpha = alpha; }
+    float getGlobalAlpha() const { return mGlobalAlpha; }
+    
+private:
+    
+    template<typename...Args>
+    struct OrderChecker {
+        OrderChecker(LayeredRenderer<Args...>& renderer, std::string layer, float order):mRenderer(renderer), mLayer(std::move(layer)), mOrder(order){}
+        template<typename T>
+        void operator ()(T&& t){
+            mRenderer.checkOrder(mLayer, mOrder, t);
+        }
+        LayeredRenderer<Args...>& mRenderer;
+        std::string mLayer;
+        float mOrder{0.f};
     };
     
-    template<typename...DrawableTypes>
-    class LayeredRenderer {
-        
-        using Layer = std::tuple<std::list<std::weak_ptr<Drawable<DrawableTypes>>>...>;
-        using LayersMap = std::map<float, Layer>;
-        
-    public:
-        
-        LayeredRenderer(Scene& scene):
-            mScene(scene),
-            mPresenter( new DefaultPresenter )
-        {
-            mScene.addDelegate<Draw>(EventDelegate::create<LayeredRenderer,&LayeredRenderer::onDraw>(this));
-            int l[] = {(addNewComponentDelegate<DrawableTypes>(),0)...};
-            (void)l;
-        }
-        
-        ~LayeredRenderer(){
-            mScene.removeDelegate<Draw>(EventDelegate::create<LayeredRenderer,&LayeredRenderer::onDraw>(this));
-            int l[] = {(removeNewComponentDelegate<DrawableTypes>(),0)...};
-            (void)l;
-        }
-        
-        template<typename Presenter, typename...Args>
-        Presenter* createPresenter( Args&&...args ){
-            static_assert(std::is_base_of<IPresenter,Presenter>::value, "Presenter must derive from IPresenter.");
-            if(mPresenter)
-                mPresenter.reset();
-            mPresenter.reset(new Presenter(std::forward<Args>(args)...));
-            return static_cast<Presenter*>(mPresenter.get());
-        }
-        
-        void draw(){
-
-            for ( auto & layer : mLayers ) {
-                for_each_in_tuple(layer.second,LayerChecker<DrawableTypes...>(*this,layer.first));
-            }
-
-            mPresenter->begin();
-            
-            for ( auto & layer : mLayers ) {
-                for_each_in_tuple(layer.second,LayerDrawer<DrawableTypes...>(*this));
-            }
-            
-            mPresenter->end();
-            mPresenter->present();
-        }
-        
-        void setGlobalAlpha(float alpha){ mGlobalAlpha = alpha; }
-        float getGlobalAlpha() const { return mGlobalAlpha; }
-        
-    private:
-        
-        template<typename...Args>
-        struct LayerChecker {
-            LayerChecker(LayeredRenderer<Args...>& renderer, float layer):mRenderer(renderer), mLayer(layer){}
-            template<typename T>
-            void operator ()(T&& t){
-                mRenderer.checkLayer(mLayer, t);
-            }
-            LayeredRenderer<Args...>& mRenderer;
-            float mLayer{0.f};
-        };
-        
-        template<typename...Args>
-        struct LayerDrawer {
-            LayerDrawer(LayeredRenderer<Args...>& renderer):mRenderer(renderer){}
-            template<typename T>
-            void operator ()(T&& t){
-                mRenderer.drawLayer(t);
-            }
-            LayeredRenderer<Args...>& mRenderer;
-        };
-        
+    template<typename...Args>
+    struct LayerDrawer {
+        LayerDrawer(LayeredRenderer<Args...>& renderer):mRenderer(renderer){}
         template<typename T>
-        void addNewComponentDelegate(){
-            mScene.addDelegate<NewComponent<Drawable<T>>>(EventDelegate::create<LayeredRenderer, &LayeredRenderer::onNewLayeredComponent<T>>(this));
+        void operator ()(T&& t){
+            mRenderer.drawLayer(t);
         }
-        
-        template<typename T>
-        void removeNewComponentDelegate(){
-             mScene.removeDelegate<NewComponent<Drawable<T>>>(EventDelegate::create<LayeredRenderer, &LayeredRenderer::onNewLayeredComponent<T>>(this));
+        LayeredRenderer<Args...>& mRenderer;
+    };
+    
+    template<typename T>
+    void addNewComponentDelegate(){
+        mScene.addDelegate<NewComponent<Drawable<T>>>(EventDelegate::create<LayeredRenderer, &LayeredRenderer::onNewLayeredComponent<T>>(this));
+    }
+    
+    template<typename T>
+    void removeNewComponentDelegate(){
+        mScene.removeDelegate<NewComponent<Drawable<T>>>(EventDelegate::create<LayeredRenderer, &LayeredRenderer::onNewLayeredComponent<T>>(this));
+    }
+    
+    template<typename T>
+    void insertIntoOrderedLayer(const std::string& layerName, float order, std::weak_ptr<Drawable<T>> handle){
+        auto found = std::find_if(mLayers.begin(), mLayers.end(), [&layerName](const Layer& layer){
+            return layer.name == layerName;
+        });
+        if(found != mLayers.end()){
+            //we have this layer, pull the typed list from the tuple at a given layer
+            auto& list = get_element_by_type<std::list<std::weak_ptr<Drawable<T>>>>(found->layer[order]);
+            list.emplace_back(std::move(handle));
+        }else{
+            //error and put it in default
+            auto& defaultLayer = *(std::find_if(mLayers.begin(), mLayers.end(), [](const Layer& layer){
+                return layer.name == "default";
+            }));
+            auto& list = get_element_by_type<std::list<std::weak_ptr<Drawable<T>>>>(defaultLayer.layer[order]);
+            list.emplace_back(std::move(handle));
+            MS_LOG_ERROR("Didnt have a rendering layer called: " + layerName + " placeing drawable in default layer.");
         }
-        
-        template<typename T>
-        void insertIntoLayer(float layer, std::weak_ptr<Drawable<T>> handle){
-            auto found = mLayers.find(layer);
-            if(found != mLayers.end()){
-                auto& list = get_element_by_type<std::list<std::weak_ptr<Drawable<T>>>>(found->second);
-                list.emplace_back(std::move(handle));
-            }else{
-                Layer l;
-                auto& list = get_element_by_type<std::list<std::weak_ptr<Drawable<T>>>>(l);
-                list.emplace_back(std::move(handle));
-                mLayers.emplace(layer, std::move(l));
-            }
-        }
-        
-        template<typename T>
-        void checkLayer(float layer, std::list<std::weak_ptr<Drawable<T>>>& handles){
-            auto it = handles.begin();
-            auto end = handles.end();
-            while( it != end ){
-                if(auto component = it->lock()){
-                    if(component->getLayer() != layer){
-                        it = handles.erase(it);
-                        insertIntoLayer<T>(component->getLayer(), std::move(component));
-                    }else{
-                        ++it;
-                    }
-                }else{
+    }
+    
+    template<typename T>
+    void checkOrder(const std::string& layer, float order, std::list<std::weak_ptr<Drawable<T>>>& handles){
+        auto it = handles.begin();
+        auto end = handles.end();
+        while( it != end ){
+            if(auto component = it->lock()){
+                if(component->getDrawOrder() != order || component->getLayer() != layer){
                     it = handles.erase(it);
-                }
-            }
-        }
-        
-        template<typename T>
-        void drawLayer(std::list<std::weak_ptr<Drawable<T>>>& handles){
-            auto it = handles.begin();
-            auto end = handles.end();
-            while (it!=end) {
-                if (auto component = (*it).lock()) {
-                    if (component->isVisible()) {
-                        auto model = component->getGlobalTransformMatrix();
-                        auto c = component->getColor();
-                        auto a = component->getAlpha();
-                        ofSetColor(ofFloatColor(c.r, c.g, c.b, a * mGlobalAlpha));
-                        ofPushMatrix();
-                        ofMultMatrix(model);
-                        component->draw();
-                        ofPopMatrix();
-                    }
+                    insertIntoOrderedLayer<T>(component->getLayer(), component->getDrawOrder(), std::move(component));
+                }else{
                     ++it;
                 }
-                else {
-                    it = handles.erase(it);
-                }
+            }else{
+                it = handles.erase(it);
             }
         }
-        
-        template<typename T>
-        EventStatus onNewLayeredComponent( const IEventRef& event ){
-            auto cast = std::static_pointer_cast<NewComponent<Drawable<T>>>(event);
-            if(cast->getComponentType() == &type_id<Drawable<T>>){
-                auto compHandle = cast->getComponentHandle();
-                if(auto comp = compHandle.lock()){
-                    auto layer = comp->getLayer();
-                    insertIntoLayer<T>(layer, std::move(compHandle));
+    }
+    
+    template<typename T>
+    void drawLayer(std::list<std::weak_ptr<Drawable<T>>>& handles){
+        auto it = handles.begin();
+        auto end = handles.end();
+        while (it!=end) {
+            if (auto component = (*it).lock()) {
+                if (component->isVisible()) {
+                    auto model = component->getGlobalTransformMatrix();
+                    auto c = component->getColor();
+                    c.a *= mGlobalAlpha;
+                    ofSetColor(c);
+                    ofPushMatrix();
+                    ofMultMatrix(model);
+                    component->draw();
+                    ofPopMatrix();
                 }
-                return EventStatus::SUCCESS;
+                ++it;
             }
-            MS_LOG_ERROR("Something is wrong, the type_ids should match on new component event!");
-            return EventStatus::FAILED;
+            else {
+                it = handles.erase(it);
+            }
         }
-        
-        EventStatus onDraw( const IEventRef& event ){
-            draw();
+    }
+    
+    template<typename T>
+    EventStatus onNewLayeredComponent( const IEventRef& event ){
+        auto cast = std::static_pointer_cast<NewComponent<Drawable<T>>>(event);
+        if(cast->getComponentType() == &type_id<Drawable<T>>){
+            auto compHandle = cast->getComponentHandle();
+            if(auto comp = compHandle.lock()){
+                auto layer = comp->getLayer();
+                auto order = comp->getDrawOrder();
+                insertIntoOrderedLayer<T>(layer, order, std::move(compHandle));
+            }
             return EventStatus::SUCCESS;
         }
-        
-        float mGlobalAlpha{1.f};
-        Scene& mScene;
-        LayersMap mLayers;
-        std::unique_ptr<IPresenter> mPresenter;
-    };
-}//end namespace
+        MS_LOG_ERROR("Something is wrong, the type_ids should match on new component event!");
+        return EventStatus::FAILED;
+    }
+    
+    EventStatus onDraw( const IEventRef& event ){
+        draw();
+        return EventStatus::SUCCESS;
+    }
+    
+    float mGlobalAlpha{1.f};
+    Scene& mScene;
+    LayerList mLayers;
+};
+    
+}//end namespace mediasystem
+
