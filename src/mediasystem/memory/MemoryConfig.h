c4b1619111b43996aabcb5c123af90824f8eee63
Michael Allison <hello@michaelallison.lol>
2019-04-13 14:53:26 -0400

    switched memory allocation to use ms pool allocators

--- src/mediasystem/memory/MemoryConfig.h
+++ src/mediasystem/memory/MemoryConfig.h
@@ -15,10 +15,13 @@
 
 namespace mediasystem {
     
-    //template<typename T>
-    //using Allocator = ms_Allocator<T>;
-        
     template<typename T>
-    using Allocator = std_Allocator<T>;
+    using Allocator = ms_Allocator<T>;
+    
+    //To configure the memory subsystem to use the std library default allocator
+    //comment out the allocator alias above and uncomment the alias below:
+    
+    //template<typename T>
+    //using Allocator = std_Allocator<T>;
     
 }//end namespace mediasystem
